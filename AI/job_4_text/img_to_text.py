fecba1cf202f2858e8f518e76a3f655c294aac7b
rain <1585918524@qq.com>
2020-10-26 17:27:26 +0800

    人工智能 图片识别文字并保存到word

---
+++ AI/job_4_text/img_to_text.py
@@ -0,0 +1,28 @@
+from aip import AipOcr
+from docx import Document
+# 图片文字识别
+""" 你的 APPID AK SK """
+APP_ID = '22855479'
+API_KEY = 'dEbfdWGaDhu7yG4h07OMaSU3'
+SECRET_KEY = 'V0hD45LqGugfCnZe9eNb6ih5cp5d7Xj4'
+
+client = AipOcr(APP_ID, API_KEY, SECRET_KEY)
+
+""" 读取图片 """
+def get_file_content(filePath):
+    with open(filePath, 'rb') as fp:
+        return fp.read()
+
+
+text = ''
+Doc = Document()
+for i in range(1, 5):
+    image = get_file_content(str(i) + '.png')
+    resp = client.basicGeneral(image);
+    words = list(map(lambda record: record['words'], resp['words_result']))
+    # print('words', words)
+    Doc.add_heading(','.join(words) + '\n')
+
+Doc.save('python_img_to_text.docx')
+
+
