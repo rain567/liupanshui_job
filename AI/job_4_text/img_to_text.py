0eda77a95e0175e040c509c6ef576877d9159cb2
rain <1585918524@qq.com>
2020-10-28 11:02:06 +0800

    人工智能 图片文字识别text补全注释

--- AI/job_4_text/img_to_text.py
+++ AI/job_4_text/img_to_text.py
@@ -1,4 +1,5 @@
 from aip import AipOcr
+
 # 图片文字识别
 """ 你的 APPID AK SK """
 APP_ID = '22855479'
@@ -7,18 +8,22 @@ SECRET_KEY = 'V0hD45LqGugfCnZe9eNb6ih5cp5d7Xj4'
 
 client = AipOcr(APP_ID, API_KEY, SECRET_KEY)
 
-""" 读取图片 """
+
+# 读取图片
 def get_file_content(filePath):
     with open(filePath, 'rb') as fp:
         return fp.read()
 
+
+# 循环获取图片并识别图片中的信息，加在text变量中
 text = ''
 for i in range(1, 5):
     image = get_file_content(str(i) + '.png')
-    resp = client.basicGeneral(image);
+    resp = client.basicGeneral(image)
     words = list(map(lambda record: record['words'], resp['words_result']))
     # print('words', words)
     text += ','.join(words) + '\n'
 
+# 输出内容到test.txt文件
 with open("test.txt", "w", encoding='utf-8') as f:
     f.write(text)
