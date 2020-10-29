92a04a82b561bd992ca62359ad8ecc7944aff338
rain <1585918524@qq.com>
2020-10-29 10:20:21 +0800

    exam.py 课堂作业修改

--- python/6_unit/exam.py
+++ python/6_unit/exam.py
@@ -1,12 +1,13 @@
 hobbies = {
-    1002: ['象棋', '五子棋', '围棋'],
-    1001: ['羽毛球', '乒乓球', '篮球'],
-    1005: ['听歌', '看书', '睡觉'],
-    1004: ['钓鱼', '旅游', '骑行'],
-    1003: ['pc game', 'handheld game', 'host game']
+    91002: ['跳绳', '五子棋', '军棋'],
+    91001: ['爬山', '乒乓球', '篮球', '游戏'],
+    91005: ['听歌', '看书', '睡觉', '看电影', '跑步'],
+    91004: ['钓鱼', '旅游', '骑行', '乐高'],
+    91003: ['pc game', 'handheld game', 'host game']
 }
 
 for employee_num in sorted(hobbies.keys(), reverse=True):
-    print('\n\n工号{}的爱好是：'.format(employee_num))
+    print('\n\n工号为{}的爱好是：'.format(employee_num))
     for hobby in hobbies[employee_num]:
         print(hobby, end='、')
+
